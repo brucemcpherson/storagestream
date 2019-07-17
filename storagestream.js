fbbc18c230f732a9ef7d4d609c35b318e0fc7667
bruce mcpherson <bruce@mcpher.com>
2019-07-17 13:33:15 +0100

    Update storagestream.js

--- storagestream.js
+++ storagestream.js
@@ -1,9 +1,9 @@
 
 const Storage = require('@google-cloud/storage').Storage;
-// make your own one of these for credentials
-const secrets = require('../private/visecrets'); 
+const secrets = require('./private/visecrets');
 var sts = require('string-to-stream');
 const request = require('request');
+const fs = require('fs');
 /**
  * create the cloud storage stream
  * the credentials/bucket name and filename are in the secrets file
@@ -29,10 +29,15 @@ const createStorageStream = async ({ name }) => {
  * convert a string or objects to a stream
  */
 const streamContent = async ({ name, content }) => {
-  const writeStream = await createWriteStream({ name });
+  let mimeType = "";
+  if(typeof content === 'object') {
+    content = JSON.stringify(content);
+    mimeType = "application/json";
+  }
+  const writeStream = await createWriteStream({ name, mimeType });
   return new Promise ((resolve, reject) => {
-    const str = typeof content === 'object' ? JSON.stringify(content) : content;
-    sts(str).pipe(writeStream).on('finish', () => {
+    console.log('streaming ', mimeType, ' to ', name);
+    sts(content).pipe(writeStream).on('finish', () => {
       resolve(name);
       console.log(name, 'uploaded to storage');
     });
@@ -66,12 +71,11 @@ const getContent = async ({ name }) => {
  */
 const createWriteStream = async ({ name, mimeType }) => {
   const blob = await createStorageStream({ name });
-  const stream = blob.createWriteStream({
-		resumable: true,
-		contentType: mimeType || blob.type
-	});
-  // this stream will be piped to
-  return stream;
+  const options = {
+		contentType: mimeType
+  };
+  console.log('writestream options', options);
+  return blob.createWriteStream(options);
 };
 /**
  * create the cloud storage stream
@@ -85,34 +89,34 @@ const createReadStream = async ({ name }) => {
   return stream;
 };
 
+
 /**
  * stream video file direct from vimeo storage to google cloud storage
  * @param {string} url the file to stream
  * @param {stream} stream the stream to pipe it to
+ * @param {stream} [mimeType] validate exprected mimeType
  */
-const downloadFile = async ({ url, stream }) => {
+const downloadFile = async ({ url, stream, mimeType }) => {
 
   return new Promise (( resolve, reject) => {
     // request the video files
-    const sendReq = request.get(url);
-    sendReq.on('response', (response) => {
-      if (response.statusCode !== 200) {
-        reject('Response status was ' + response.statusCode);
-      } else {
-        // and pipe the output to the blob stream for cloud storage
-        sendReq.pipe(stream);
-      }
-    });
-    // close when done
-    sendReq.on('end', () => {
-      stream.end();
-      resolve(url);
-    });
+    console.log('requesting download from', url);
 
-    // need to report any errors
-    sendReq.on('error', err => {
-        reject(err.message);
-    });
+    // request(url)
+    request.get(url)
+    .on('error', err => reject(err))
+    .on('response', response => {
+      if (response.statusCode !== 200) {
+        reject('unexpected status code:' + response.statusCode);
+      }
+      // if required, check mimetype is what was expected
+      if (mimeType && response.headers['content-type'] !== mimeType) {
+        reject('expected:' + mimeType + ' got:'  + response.headers['content-type']);
+      }
+    })
+    .pipe(stream)
+    .on('error', err => reject(err))
+    .on('finish', () => resolve(url));
   });
 };
 module.exports = {
